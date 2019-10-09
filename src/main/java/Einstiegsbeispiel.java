8b17ebc05270ae14ac69a44e85d305925f924b01
Klemens Vospernik <klemens.vospernik@gmx.at>
2019-10-09 20:42:10 +0200

    commit with hello world, working now

--- src/main/java/Einstiegsbeispiel.java
+++ src/main/java/Einstiegsbeispiel.java
@@ -1,5 +1,7 @@
 public class Einstiegsbeispiel {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("This is me again!");
     }
+
 }
--- src/test/java/EinstiegsbeispielTest.java
+++ src/test/java/EinstiegsbeispielTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class EinstiegsbeispielTest {
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    public void test2() {
+        Assertions.assertTrue(true);
+    }
 }
